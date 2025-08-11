49169e62955dd02c93bd319624df1eab571fc4c8
Moneer <xv.neer.business@gmail.com>
2025-08-11 13:20:28 +0300

    init next intl for multi language support

---
+++ app/[locale]/page.tsx
@@ -0,0 +1,12 @@
+import { useTranslations } from "next-intl"
+import { Link } from "@/lib/i18n/navigation"
+
+export default function HomePage() {
+  const t = useTranslations("home-page")
+  return (
+    <div>
+      <h1>{t("title")}</h1>
+      <Link href="/about">{t("about")}</Link>
+    </div>
+  )
+}
